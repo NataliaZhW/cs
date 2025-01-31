53370a1e1020584aa684b4080e83644d618da55b
NataliaZhW <zhuravleva.natalia@rambler.ru>
2025-01-31 17:31:23 +0300

    Игра без фальстарта

---
+++ 20.01/Program_20_01_25_game.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Приготовьтесь!");
+            Thread.Sleep(2000); 
+            while (Console.KeyAvailable) Console.ReadKey(true);//Отсекаем "Фальстарт!" 
+            Console.WriteLine("Жми!");
+            DateTime time1 = DateTime.Now;
+            Console.WriteLine(time1);
+            //Console.ReadKey(false);
+            Console.ReadKey(true);
+            DateTime time2 = DateTime.Now;
+            Console.WriteLine(time2);
+            TimeSpan timeRez = time2 - time1;
+            Console.WriteLine($"Получилось за {timeRez.TotalMilliseconds} мс");        }
+    }
+}
