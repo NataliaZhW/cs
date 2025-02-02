996dd0df42e2a10967b9fd678bdcf5fa7b45b88a
NataliaZhW <zhuravleva.natalia@rambler.ru>
2025-02-02 16:44:48 +0300

    PoolApplication

--- Sources2/PoolApplication/Backup/Backup/PoolApplication/Program.cs
+++ Sources2/PoolApplication/Backup/Backup/PoolApplication/Program.cs
@@ -10,7 +10,7 @@ namespace PoolApplication
             Console.WriteLine("Основной поток: ставим в очередь рабочий элемент");
             Random r = new Random();
             for (int i = 0; i < 10; ++i)
-                ThreadPool.QueueUserWorkItem(WorkingElementMethod, i); //r.Next(10), чтобы был виден порядок/беспорядок передачи 
+                ThreadPool.QueueUserWorkItem(WorkingElementMethod, r.Next(10));
             Console.WriteLine("Основной поток: выполняем другие задачи");
             Thread.Sleep(1000);
             Console.WriteLine("Нажмите любую клавишу для продолжения...");
