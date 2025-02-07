851311a84615fe0a9b9774a6d7db03ccf6f0a51a
NataliaZhW <zhuravleva.natalia@rambler.ru>
2025-02-07 20:59:27 +0300

    WinFormsApp3 07_02_25

--- 29.01/WinFormsApp2/obj/Debug/net8.0-windows/WinFormsApp2.AssemblyInfo.cs
+++ 29.01/WinFormsApp2/obj/Debug/net8.0-windows/WinFormsApp2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0faf85bdc15a6c0a09c3bd0f74d8fe2dff4f8ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8520445157bc147d72301ad038cd1062f533e1a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
